decb1e8e46301657c2a463c278fd2e4333ab3e3b
Taban Baghfalaki <64243070+tbaghfalaki@users.noreply.github.com>
2024-02-15 09:35:56 +0100

    Update G_legendre.R

--- G_legendre.R
+++ G_legendre.R
@@ -1,5 +1,5 @@
 library(statmod)
-glq <- gauss.quad(15, kind = "legendre")
+glq <- gauss.quad(15, kind = "legendre")### kind of Gaussian quadrature, one of "legendre", "chebyshev1", "chebyshev2", "hermite", "jacobi" or "laguerre"
 xk <- glq$nodes # Nodes
 wk <- glq$weights # Weights
 K <- length(xk) # K-points
